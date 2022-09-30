af523557cf3ac335d59ecf0d1d529b0141c68a49
Codedien <nguyentanminh1607@gmail.com>
2022-09-30 11:58:32 +0700

    update bai tap mang

---
+++ Minh_A0822I1_Baitap/Module1/bai_tap/Baitap_mang/Luyentap_mang/node.js
@@ -0,0 +1,5 @@
+let myArrayA=[1,4,46,-12,9,11,-6,646,-123,45,6]
+let myArrayB=[-6,15,6,123,-27,30,1,19,303,66,100]
+let myArrayC=[1,4,46,-12,9,15,-6,646,-123,135,6,-6,15,6,123,-27,30,1,19,303,5]
+let add=myArrayB.concat(myArrayA);
+console.log(myArrayC.concat(add))
