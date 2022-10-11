aecdcf30ef48b30634b8c2702b9b1619f8e57613
Codedien <nguyentanminh1607@gmail.com>
2022-10-12 06:42:38 +0700

    update

---
+++ Minh_A0822I1_Baitap/Module1/bai_tap/baitap_lamthem/caseStudy/test.js
@@ -0,0 +1,12 @@
+let a = [1, 2, 3]
+let b = [];
+let c = 0;
+for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a[i]; j++) {
+        b[i] = a[i];
+        c = c + 1;
+        b[i]=c;
+
+    }
+
+}console.log(b)
